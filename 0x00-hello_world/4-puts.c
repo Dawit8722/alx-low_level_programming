10057818cecff10e3670e5d3723fa8ec82dc756b
dawit8722 <dawit8722@gmail.com>
2021-02-03 19:27:25 +0000

    new

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main()
 {
-  strcpy(str1, "Programming is like building a multilingual puzzle");
+  strcpy(str1, "Programming is like building a multilingual puzzle")\n;
   puts(str1);
   return(0);
 }
