e876fbd35534017946c0e9c2e668b9fc58390453
dawit8722 <dawit8722@gmail.com>
2021-02-04 06:07:13 +0000

    script that uses puts to print string

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 /**
-*main - general function that contains the entire code 
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
