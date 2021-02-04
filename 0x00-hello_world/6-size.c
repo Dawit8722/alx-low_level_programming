9e4d1126ee16a3dfec58bbf56ca07ce9d5d8c2ba
dawit8722 <dawit8722@gmail.com>
2021-02-04 08:48:38 +0000

    Program that prints out the size of various types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,10 +13,10 @@ long longType;
 long long longlongType;
 char charType;
 /* Sizeof operator is used to evaluate the size of a variable*/
-printf("Size of a char: %zu byte\n", sizeof(charType));
+printf("Size of a char: %zu byte(s)\n", sizeof(charType));
 printf("Size of an int: %zu byte(s)\n", sizeof(integerType));
-printf("Size of a long: %zu byte\n", sizeof(longType));
-printf("Size of a long long: %zu byte\n", sizeof(longlongType));
+printf("Size of a long: %zu byte(s)\n", sizeof(longType));
+printf("Size of a long long: %zu byte(s)\n", sizeof(longlongType));
 printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
 return (0);
 }
