db358b935a81784122e76bb4d09cf2b7343f2069
dawit8722 <dawit8722@gmail.com>
2021-02-04 16:11:06 +0000

    No error on betty

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,12 +3,17 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+/**
+ *main - entry point
+ *
+ *Return: 0 Success character
+ */
 int main(void)
 {
-  int n;
+int n;
 
-  srand(time(0));
-  n = rand() - RAND_MAX / 2;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
   /* your code goes there */
-  return (0);
+return (0);
 }
