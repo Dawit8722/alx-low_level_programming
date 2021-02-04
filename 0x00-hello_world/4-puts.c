0c7932e6608ec318950eb1a1e7bbae08119452d0
dawit8722 <dawit8722@gmail.com>
2021-02-04 05:36:55 +0000

    Using puts to pring a string

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
+/*Script that uses the puts function to print out strings*/
 #include <stdio.h>
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
