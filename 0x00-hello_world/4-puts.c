92a4a74e26fa5e686ed2fc4882e789b6eefa49ba
dawit8722 <dawit8722@gmail.com>
2021-02-04 05:07:12 +0000

    Using puts to pring a string

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(void)
 {
-  fputs("Programming is like building a multilingual puzzle", stdout)
+  puts("Programming is like building a multilingual puzzle", stdout);
   getchar();
   return 0;
 }
